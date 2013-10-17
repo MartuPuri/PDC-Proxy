dde9dfe779b5881c32675ee2ee21a67c39549dbc
Facundo Menzella <facumenzella@gmail.com>
2013-10-17 18:44:36 -0300

    "HttpResponse, HttpRequest 2nd commit"

--- src/main/java/itba/pdc/model/HttpRequest.java
+++ src/main/java/itba/pdc/model/HttpRequest.java
@@ -1,5 +1,101 @@
 package itba.pdc.model;
 
-public interface HttpRequest {
-	public boolean hasBody();
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class HttpRequest {
+
+	private String method;
+	private String body;
+	private int[] version;
+	private Map<String, String> params;
+	private Map<String, String> headers;
+	private static final Set<String> supportedMethods = createMethods();
+	private static final Set<String> supportedHeaders = createHeaders();
+
+	private static Set<String> createHeaders() {
+		Set<String> headers = new HashSet<String>();
+		headers.add("Accept");
+		headers.add("Accept-Charset");
+		headers.add("Accept-Encoding");
+		headers.add("Accept-Language");
+		headers.add("Accept-Datetime");
+		headers.add("Authorization");
+		headers.add("Cache-Control");
+		headers.add("Connection");
+		headers.add("Cookie");
+		headers.add("Content-Length");
+		headers.add("Content-MD5");
+		headers.add("Content-Type");
+		headers.add("Date");
+		headers.add("Expect");
+		headers.add("From");
+		headers.add("Host");
+		headers.add("If-Match");
+		headers.add("If-None-Match");
+		headers.add("If-Modified-Since");
+		headers.add("If-Range");
+		headers.add("Max-Forwards");
+		headers.add("If-Unmodified-Since");
+		headers.add("Origin");
+		headers.add("Pragma");
+		headers.add("Proxy-Authorization");
+		headers.add("Range");
+		headers.add("Referer");
+		headers.add("TE");
+		headers.add("Upgrade");
+		headers.add("User-Agent");
+		headers.add("Via");
+		headers.add("Warning");
+
+		return headers;
+	}
+	private static Set<String> createMethods(){
+		Set<String> headers = new HashSet<String>();
+		headers.add("GET");
+		headers.add("POST");
+		headers.add("HEAD");
+		return headers;
+	}
+	
+	public HttpRequest(){
+		this.params = new HashMap<String,String>();
+		this.headers = new HashMap<String,String>();
+		this.version = new int[2];
+	}
+	
+	public void addHeader(String header, String value) {
+		if (!supportedHeaders.contains(header)) {
+			System.out.println("Invalid header");
+			// TODO: VER QUE HACEMOS
+		}
+		headers.put(header, value);
+	}
+
+	public void setVersion(int[] version) {
+		if(version[0] != 1 || version[1] != 1){
+			System.out.println("Invalid version");
+			//TODO: VER QUE HACER
+		}
+		this.version[0] = version[0];
+		this.version[1] = version[1];
+	}
+
+	public void setMethod(String method) {
+		if(!supportedMethods.contains(method)){
+			System.out.println("Invalid method");
+			//TODO: VER QUE HACEMOS
+		}
+		this.method = method;
+	}
+	
+	public void setParams(Map<String,String> params){
+			this.params.putAll(params);
+	}
+
+	public void setBody(String body){
+		this.body = body;
+	}
 }
--- src/main/java/itba/pdc/model/HttpResponse.java
+++ src/main/java/itba/pdc/model/HttpResponse.java
@@ -1,5 +1,154 @@
 package itba.pdc.model;
 
-public interface HttpResponse {
+import java.text.SimpleDateFormat;
+import java.util.Collections;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class HttpResponse {
+	
+	private int status;
+	private String body;
+	private Map<String,String> headers;
+	
+	private static final Map<Integer, String> httpReplies = createHttpReplies();
+	private static final Set<String> supportedHeaders = createHeaders();
+    
+	public HttpResponse(){
+	}
+	private static Set<String> createHeaders(){
+		Set<String> headers = new HashSet<String>();
+		headers.add("Access-Control-Allow-Origin");
+		headers.add("Accept-Ranges");
+		headers.add("Age");
+		headers.add("Allow");
+		headers.add("Cache-Control");
+		headers.add("Connection");
+		headers.add("Content-Encoding");
+		headers.add("Content-Language");
+		headers.add("Content-Length");
+		headers.add("Content-Location");
+		headers.add("Content-MD5");
+		headers.add("Content-Type");
+		headers.add("Content-Disposition");
+		headers.add("Content-Range");
+		headers.add("Content-Type");
+		headers.add("Date");
+		headers.add("ETag");
+		headers.add("Expires");
+		headers.add("Last-Modified");
+		headers.add("Link");
+		headers.add("Location");
+		headers.add("P3P");
+		headers.add("Pragma");
+		headers.add("Proxy-Authenticate");
+		headers.add("Refresh");
+		headers.add("Retry-After");
+		headers.add("Server");
+		headers.add("Set-Cookie");
+		headers.add("Status");
+		headers.add("Strict-Transport-Security");
+		headers.add("Trailer");
+		headers.add("Transfer-Encoding");
+		headers.add("Vary");
+		headers.add("Via");
+		headers.add("Warning");
+		headers.add("WWW-Authenticate");
+		
+		return headers;
+	}
+	private static Map<Integer, String> createHttpReplies() {
+        Map<Integer, String> result = new HashMap<Integer, String>();
+        result.put(100, "Continue");
+        result.put(101, "Switching Protocols");
+        result.put(200, "OK");
+        result.put(201, "Created");
+        result.put(202, "Accepted");
+        result.put(203, "Non-Authoritative Information");
+        result.put(204, "No Content");
+        result.put(205, "Reset Content");
+        result.put(206, "Partial Content");
+        result.put(300, "Multiple Choices");
+        result.put(301, "Moved Permanently");
+        result.put(302, "Found");
+        result.put(303, "See Other");
+        result.put(304, "Not Modified");
+        result.put(305, "Not Modified");
+        result.put(306, "(Unused)");
+        result.put(307, "Temporary Redirect");
+        result.put(400, "Bad Request");
+        result.put(401, "Unauthorized");
+        result.put(402, "Payment Required");
+        result.put(403, "Forbidden");
+        result.put(404, "Not Found");
+        result.put(405, "Method Not Allowed");
+        result.put(406, "Not Acceptable");
+        result.put(407, "Proxy Authentication Required");
+        result.put(408, "Request Timeout");
+        result.put(409, "Conflict");
+        result.put(410, "Gone");
+        result.put(411, "Length Required");
+        result.put(412, "Precondition Failed");
+        result.put(413, "Request Entity Too Large");
+        result.put(414, "Request-URI Too Long");
+        result.put(415, "Request-URI Too Long");
+        result.put(416, "Requested Range Not Satisfiable");
+        result.put(417, "Expectation Failed");
+        result.put(500, "Internal Server Error");
+        result.put(501, "Not Implemented");
+        result.put(502, "Bad Gateway");
+        result.put(503, "Service Unavailable");
+        result.put(504, "Gateway Timeout");
+        result.put(505, "Http Version Not Supported");
+        
+        return Collections.unmodifiableMap(result);
+    }
+
+	public String getStatusValue(){
+		if(!httpReplies.containsKey(this.status)){
+			//TODO: VER QUE HACEMOS
+		}
+		return httpReplies.get(this.status);
+	}
+	
+	public void setStatus(int status){
+		if(!httpReplies.containsKey(status)){
+			System.out.println("Invalid Status");
+			//TODO: VER QUE HACER
+		}
+		this.status = status;
+	}
+
+	public void addHeader(String header, String value) {
+		if (!supportedHeaders.contains(header)) {
+			System.out.println("Invalid header");
+			// TODO: VER QUE HACEMOS
+		}
+		headers.put(header, value);
+	}
+
+	public void setBody(String body){
+		this.body = body;
+	}
+
+	private void addDefaultHeaders(){
+		this.headers.put("Date", new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").format(new Date()));
+		this.headers.put("Status", this.status + " " + this.getStatusValue());
+	}
+	
+	private void unsopportedMethod(){
+		this.status = 405;
+		this.headers.put("Allow", "GET,POST,HEAD");
+		this.addDefaultHeaders();
+	}
+	
+	private void badRequest(){
+		this.status = 400;
+		this.addDefaultHeaders();
+	}
+
 
 }
